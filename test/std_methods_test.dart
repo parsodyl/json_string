6d900c84f0d585731af05ac5b05bc4a0bd70baad
Daniele Paradiso (mac) <daniele.paradiso3@gmail.com>
2019-01-20 15:44:21 +0100

    add: == unit tests

---
+++ test/std_methods_test.dart
@@ -0,0 +1,54 @@
+import 'package:test/test.dart';
+import 'package:json_string/json_string.dart';
+
+void main() {
+  setUp(() {
+    // setup
+  });
+  tearDown(() {
+    // teardown
+  });
+  test(
+    'success test: compare JsonString objects #1 (same source string)',
+    () {
+      // prepare input
+      final source = '{"username":"john_doe","password":"eiaw"}';
+      // execute
+      final jsonString1 = JsonString(source);
+      final jsonString2 = JsonString(source);
+      // check
+      expect(jsonString1, equals(jsonString2));
+    },
+  );
+  test(
+    'success test: compare JsonString objects #2 (same content)',
+    () {
+      // prepare input
+      final source1 = '''
+      {
+        "username": "john_doe",
+        "password": "eiaw"
+      }
+      ''';
+      final source2 = '{"username":"john_doe","password":"eiaw"}';
+      // execute
+      final jsonString1 = JsonString(source1);
+      final jsonString2 = JsonString(source2);
+      // check
+      expect(jsonString1, equals(jsonString2));
+    },
+  );
+  test(
+    'fail test: compare JsonString objects #3 (same content, different order)',
+    () {
+      // prepare input
+      final source1 = '{"username":"john_doe","password":"eiaw"}';
+      final source2 = '{"password":"eiaw", "username":"john_doe"}';
+      // execute
+      final jsonString1 = JsonString(source1);
+      final jsonString2 = JsonString(source2);
+      // check
+      expect(jsonString1, isNot(equals(jsonString2)));
+    },
+  );
+}
