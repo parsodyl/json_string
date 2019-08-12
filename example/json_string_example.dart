ced45bbfc339cbb29186fb4f82fb6cb27cf23642
Daniele Paradiso (mac) <daniele.paradiso3@gmail.com>
2019-08-12 19:02:44 +0200

    update: pub example

--- example/json_string_example.dart
+++ example/json_string_example.dart
@@ -1,18 +1,22 @@
 import 'package:json_string/json_string.dart';
+import 'package:collection/collection.dart';
 
 void main() {
   const externalSource = '{"username":"john_doe","password":"querty"}';
   var jsonString = JsonString.orNull(externalSource);
-  if(jsonString == null) {
+  if (jsonString == null) {
     print("Bad source!");
   }
-  final user1 = User.fromJson(jsonString.decodedValue);
+  final user1 = jsonString.decodedValueAsObject(User.fromJson);
   final user2 = User(username: 'clara_brothers', password: 'asdfgh');
   final userList = [user1, user2];
-  
+
   jsonString = JsonString.encodeObjectList(userList);
   final internalSource = jsonString.source;
   print(internalSource);
+
+  final decodedList = jsonString.decodedValueAsObjectList(User.fromJson);
+  print(ListEquality().equals(decodedList, userList)); // true
 }
 
 class User with Jsonable {
@@ -24,15 +28,21 @@ class User with Jsonable {
     this.password,
   });
 
-  static User fromJson(Map<String, dynamic> json) {
-    return User(
-      username: json['username'],
-      password: json['password'],
-    );
-  }
+  static User fromJson(Map<String, dynamic> json) => User(
+        username: json['username'],
+        password: json['password'],
+      );
 
   Map<String, dynamic> toJson() => {
         'username': username,
         'password': password,
       };
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is User &&
+          runtimeType == other.runtimeType &&
+          username == other.username &&
+          password == other.password;
 }
