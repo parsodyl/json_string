84aa9373d62a47692cc7e06b154d824a1fc66efe
Daniele Paradiso (mac) <daniele.paradiso3@gmail.com>
2019-09-20 08:24:45 +0200

    update: json_string lib file

--- lib/json_string.dart
+++ lib/json_string.dart
@@ -1,6 +1,10 @@
+// Copyright (c) 2018, Daniele Paradiso.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 library json_string;
 
-export 'src/json_string.dart' show JsonString;
-export 'src/functions.dart';
 export 'src/errors.dart';
+export 'src/functions.dart';
+export 'src/json_string.dart' show JsonString;
 export 'src/jsonable.dart';
