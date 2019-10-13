f28bd0928826ef0fbed242c27f9051e86717f560
Daniele Paradiso (mac) <daniele.paradiso3@gmail.com>
2019-10-13 15:02:53 +0200

    update: version number + minor corrections

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.1
+
+* Documentation corrected.
+
 ## 1.0.0
 
 **Breaking:**
--- lib/json_string.dart
+++ lib/json_string.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
+/// A simple and lightweight JSON data container for Dart and Flutter.
 library json_string;
 
 export 'src/errors.dart';
